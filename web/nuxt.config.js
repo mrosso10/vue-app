5a803e24041e0a41d87ecbbbeb723f2af1320153
Martín Rosso <mrosso10@gmail.com>
2020-11-07 16:02:52 -0300

    Page transitions

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/main.scss'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
