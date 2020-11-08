c0c54140ec442383044f8af5715e5247c38c91a4
Martín Rosso <mrosso10@gmail.com>
2020-11-08 03:47:08 -0300

    Cargar proyectos por ajax

--- web/pages/proyectos/index.vue
+++ web/pages/proyectos/index.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
-  .proyectos
-    h1 Proyectos ({{count}})
-    v-simple-table
-      tbody
-        tr(v-for="proyecto in proyectos")
-          td {{ proyecto.nombre }}
-          td
-            v-btn(text
-                  color="error"
-                  @click="borrar(proyecto)")
-              v-icon mdi-delete
-    v-btn(to="/proyectos/new")
-      v-icon(dark) mdi-plus
-      | Crear
+  div
+    v-btn(@click="get_proyectos")
+      | Re-cargar
+    .proyectos
+      h1 Proyectos ({{count}})
+      v-simple-table
+        tbody
+          tr(v-for="proyecto in proyectos")
+            td {{ proyecto.nombre }}
+            td
+              v-btn(text
+                    color="error"
+                    @click="borrar(proyecto)")
+                v-icon mdi-delete
+      v-btn(to="/proyectos/new")
+        v-icon(dark) mdi-plus
+        | Crear
 </template>
 
 <style>
@@ -22,16 +25,30 @@ table {
 </style>
 
 <script>
+import { create } from 'apisauce'
+
 export default {
+  data: () => ({
+    proyectos: [],
+  }),
+
   computed: {
-    proyectos() {
-      return this.$store.state.proyectos.all
-    },
     count() {
-      return this.$store.state.proyectos.all.length
+      return this.proyectos.length
     },
   },
+  created() {
+    this.get_proyectos()
+  },
   methods: {
+    get_proyectos() {
+      const api = create({
+        baseURL: 'https://private-b26ea7-programando.apiary-mock.com',
+        headers: { Accept: 'application/json' },
+      })
+
+      api.get('/proyectos').then((response) => (this.proyectos = response.data))
+    },
     borrar(proyecto) {
       this.$store.commit('proyectos/remove', proyecto)
     },
