962fdd9708b433c29fe027a0b9e7193b240ead5a
Martín Rosso <mrosso10@gmail.com>
2020-11-14 03:39:29 -0300

    más tests

--- pages/proyectos/new.vue
+++ pages/proyectos/new.vue
@@ -18,8 +18,8 @@ form
 
   //- v-select(v-model='select' :items='items' :error-messages='selectErrors' label='Item' required='' @change='$v.select.$touch()' @blur='$v.select.$touch()')
   //- v-checkbox(v-model='checkbox' :error-messages='checkboxErrors' label='Do you agree?' required='' @change='$v.checkbox.$touch()' @blur='$v.checkbox.$touch()')
-  v-btn.mr-4(@click='submit') submit
-  v-btn.mr-4(@click='clear') clear
+  v-btn.mr-4(@click='submit' ref="submit") submit
+  v-btn.mr-4(@click='clear' ref="clear") clear
   v-btn(to="/proyectos") volver
 
   //- p Nombre: {{ name }}
@@ -93,8 +93,8 @@ export default {
   methods: {
     submit() {
       this.$v.$touch()
-      this.$store.commit('proyectos/add', { nombre: this.name })
-      this.$router.push({ name: 'proyectos' })
+      // this.$store.commit('proyectos/add', { nombre: this.name })
+      // this.$router.push({ name: 'proyectos' })
     },
     clear() {
       this.$v.$reset()
--- test/proyectos/new.spec.js
+++ test/proyectos/new.spec.js
@@ -1,20 +1,45 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import { shallowMount } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import { mount, createLocalVue } from '@vue/test-utils'
 import New from '@/pages/proyectos/new.vue'
 
+// import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+
+// shallowMount(Component, {
+//   localVue,
+//   router
+// })
+
 Vue.use(Vuetify)
+Vue.use(VueRouter)
 
 describe('New', () => {
-  it('mock adapter', (done) => {
-    const wrapper = shallowMount(New)
-    setTimeout(() => {
-      try {
-        expect(wrapper.vm.items.length).toBe(2)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    }, 0)
+  // it('mock adapter', () => {
+  //   const wrapper = mount(New)
+  //   expect(wrapper.vm.items.length).toBe(2)
+  // })
+  it('mock adapter', async () => {
+    const wrapper = mount(New, {
+      localVue,
+      router,
+    })
+    // const botones = wrapper.findAll('button').at(0)
+    // const submit = wrapper.findComponent({ ref: 'submita' })
+    await wrapper.findComponent({ ref: 'submit' }).trigger('click')
+    await wrapper.findComponent({ ref: 'clear' }).trigger('click')
+    // done()
+    // setTimeout(() => {
+    //   try {
+    //     // expect(botones).toHaveLength(2)
+    //     done()
+    //   } catch (error) {
+    //     done(error)
+    //   }
+    // }, 2000)
   })
 })
