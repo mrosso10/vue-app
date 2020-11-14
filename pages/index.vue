66a9b12af6ed2887472d175d121ba6c8ca081c2c
Martín Rosso <mrosso10@gmail.com>
2020-11-14 03:50:08 -0300

    borro logos

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+        <!-- <logo />
+        <vuetify-logo /> -->
       </div>
       <v-card>
         <v-card-title class="headline">
@@ -79,15 +79,3 @@
     </v-flex>
   </v-layout>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
-}
-</script>
